e88a21214739fb3d8d666482e01cb4511932e970
aaron <aaron.kane@slq.qld.gov.au>
2025-01-07 22:10:36 +1000

    start again

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,18 @@
-import functions from 'firebase-functions'
-import next from 'next'
-import admin from 'firebase-admin'
+import * as functions from 'firebase-functions';
+import next from 'next';
+import * as admin from 'firebase-admin';
 
+// Initialize Firebase Admin SDK
 admin.initializeApp();
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, conf: { distDir: '.next' } })
-const handle = app.getRequestHandler()
+// Initialize Next.js
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev, conf: { distDir: '.next' } });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
+  // Firebase function to handle all requests
   exports.nextApp = functions.https.onRequest((req, res) => {
-    return handle(req, res)
-  })
-})
+    return handle(req, res);
+  });
+});
