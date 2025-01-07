685b10ad3e359949b4bd46f60fe79ebfc97de749
aaron <aaron.kane@slq.qld.gov.au>
2025-01-07 22:34:15 +1000

    okok

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack(config) {
+  webpack(config, {isServer}) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"]
     });
+
+    if (isServer) {
+      // Exclude the functions folder from the server-side build
+      config.externals = [
+        ...(config.externals || []),
+        /\/functions\//,  // You can use the actual path for your functions folder
+      ];
+    }
 
     return config
   }};
